e87f211e28471c478b036189bba2fda0a379004b
Jules DANZEL d'AUMONT <julesdanzel80@orange.fr>
2025-01-30 22:05:57 +0000

    dev of the shiny app

--- app.py
+++ app.py
@@ -1,78 +1,53 @@
 import pandas as pd
 import json
 from shiny import App, render, ui, reactive
+import matplotlib.pyplot as plt
+import faicons as fa
 
 with open('database.json', 'r') as json_input:
     json_db = json.load(json_input)
 df = pd.DataFrame(json_db["trainList"])
 
-
-"""
-delay_mean = df['Delay'].mean()
-delay_median = df['Delay'].median()
-price_total = df['Price'].sum()
-distance_total = df['Distance'].sum()
-speed_mean = df['Speed'].mean()
-traveltime_total = df['TravelTime'].sum()
-
 app_ui = ui.page_fluid(
-    ui.input_select("filter_by", "Filtrer par", ["Origine", "Destination", "Type"]),
-    ui.output_text("delay_mean"),
-    ui.output_text("delay_median"),
-    ui.output_text("price_total"),
-    ui.output_text("distance_total"),
-    ui.output_text("speed_mean"),
-    ui.output_text("traveltime_total")
-)
-
-def server(input, output, session):
-    @output
-    @render.text
-    def delay_mean():
-        filtered_df = df[df[input.filter_by] == input.filter_value]
-        return f"Delay moyen: {filtered_df['Delay'].mean()}"
-
-    @output
-    @render.text
-    def delay_median():
-        filtered_df = df[df[input.filter_by] == input.filter_value]
-        return f"Delay médian: {filtered_df['Delay'].median()}"
-
-    @output
-    @render.text
-    def price_total():
-        filtered_df = df[df[input.filter_by] == input.filter_value]
-        return f"Price total: {filtered_df['Price'].sum()}"
-
-    @output
-    @render.text
-    def distance_total():
-        filtered_df = df[df[input.filter_by] == input.filter_value]
-        return f"Distance totale: {filtered_df['Distance'].sum()}"
-
-    @output
-    @render.text
-    def speed_mean():
-        filtered_df = df[df[input.filter_by] == input.filter_value]
-        return f"Speed moyenne: {filtered_df['Speed'].mean()}"
-
-    @output
-    @render.text
-    def traveltime_total():
-        filtered_df = df[df[input.filter_by] == input.filter_value]
-        return f"TravelTime total: {filtered_df['TravelTime'].sum()}"
-
-"""
-app_ui = ui.page_fluid(
-    ui.input_selectize("year", "Select Year", choices=["All"] + sorted(df["Year"].unique().tolist())),
-    ui.output_text("ntrain"),
-    ui.output_text("distance"),
-    ui.output_text("time"),
-    ui.output_text("speed"),
-    ui.output_text("totaldelay"),
-    ui.output_text("meandelay"),
-    ui.output_text("mediandelay")
-)
+    ui.layout_sidebar(ui.sidebar(
+            ui.input_selectize("year", "Select Year", choices=["All"] + sorted(df["Year"].unique().tolist())),
+            width=300),
+    ui.layout_columns(
+        ui.value_box(
+            "Number of train taken", ui.output_ui("ntrain"), showcase=fa.icon_svg("train")
+        ),
+        ui.value_box(
+            "Total distance travelled", ui.output_ui("distance"), showcase=fa.icon_svg("route")
+        ),
+        ui.value_box(
+            "Total time in train",
+            ui.output_ui("time"),
+            showcase=fa.icon_svg("clock", "regular"),
+        ),
+        fill=False,
+    ),
+    ui.layout_columns(
+        ui.value_box(
+            "Average speed", ui.output_ui("speed"), showcase=fa.icon_svg("gauge-high")
+        ),
+        ui.value_box(
+            "Station visited", ui.output_ui("nstation"), showcase=fa.icon_svg("location-dot")
+        ),
+        ui.value_box(
+            "Total delay",
+            ui.output_ui("totaldelay"),
+            showcase=fa.icon_svg("stopwatch-20"),
+        ),
+        fill=False,
+    ),
+    #ui.output_text("meandelay"),
+    ui.output_text("mediandelay"),
+    ui.output_text("maxdelay"),
+    ui.output_text("averagerelative"),
+    ui.output_text("medianrelative"),
+    ui.output_text("maxrelative"),
+    ui.output_plot("traintaken_pl")
+))
 
 # Define the server logic
 def server(input, output, session):
@@ -81,13 +56,13 @@ def server(input, output, session):
         if input.year() == "All":
             return df
         else:
-            return df[df["Year"] == input.year()]
+            return df[df["Year"] == int(input.year())]
 
     @output
     @render.text
     def ntrain():
         total = filtered_data().shape[0]
-        return f"Total train taken: {total}"
+        return total
     
     @output
     @render.text
@@ -96,7 +71,7 @@ def server(input, output, session):
         days = total // (24 * 60)
         hours = (total % (24 * 60)) // 60
         minutes = total % 60
-        return f"Total time travelled: {days} days, {hours} hours and {minutes} minutes"
+        return f"{days}d, {hours}h & {minutes}mins"
     
     @output
     @render.text
@@ -105,13 +80,13 @@ def server(input, output, session):
         days = total // (24 * 60)
         hours = (total % (24 * 60)) // 60
         minutes = total % 60
-        return f"Total delay: {days} days, {hours} hours and {minutes} minutes"
+        return f"{days}d, {hours}h & {minutes}mins"
     
     @output
     @render.text
     def speed():
         total = round(filtered_data()["Distance"].mean(), 2)
-        return f"Average speed: {total} km/h"
+        return f"{total} km/h"
     
     @output
     @render.text
@@ -127,9 +102,65 @@ def server(input, output, session):
     
     @output
     @render.text
+    def maxdelay():
+        total = filtered_data()["Delay"].max()
+        return f"Max delay: {total} min"
+    
+    @output
+    @render.text
+    def nstation():
+        total = filtered_data()
+        unique_values = pd.concat([df['Origin'], df['Destination']]).nunique()
+        return unique_values
+    
+    @output
+    @render.text
     def distance():
         total = round(filtered_data()["Distance"].sum(), 2)
-        return f"Distance travelled: {total} km"
+        return f"{total} km"
+
+    @output
+    @render.text
+    def averagerelative():
+        total = round(filtered_data()["RelativeDuration"].mean(), 2)
+        return f"Average relative duration: {total} %"
+    
+    @output
+    @render.text
+    def medianrelative():
+        total = filtered_data()["RelativeDuration"].median()
+        return f"Mean relative duration: {total} %"
+    
+    @output
+    @render.text
+    def maxrelative():
+        total = filtered_data()["RelativeDuration"].max()
+        return f"Max relative duration: {total} %"
+    
+    @output
+    @render.plot
+    def traintaken_pl():
+        data = filtered_data()
+        
+        if input.year() == "All":
+            grouped_data = data.groupby(["Year", "Month"]).size().reset_index(name="count")
+            grouped_data["year_month"] = grouped_data["Year"].astype(str) + "-" + grouped_data["Month"].astype(str).str.zfill(2)
+            x_axis = grouped_data["year_month"]
+            title = "Train taken by month"
+        else:
+            grouped_data = data.groupby("Month").size().reset_index(name="count")
+            x_axis = grouped_data["Month"]
+            title = "Train taken by month"
+
+        fig, ax = plt.subplots()
+        ax.bar(x_axis, grouped_data["count"], color="skyblue")
+        ax.set_xlabel("Month")
+        ax.set_ylabel("Number of train taken")
+        ax.set_title(title)
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        return fig
 
 
 
